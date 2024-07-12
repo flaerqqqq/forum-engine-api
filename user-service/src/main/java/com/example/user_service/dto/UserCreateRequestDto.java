06a320b59304932735bea8ab30297ffe3ccd3887
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-12 21:19:54 +0300

    modified user related dtos

--- user-service/src/main/java/com/example/user_service/dto/UserCreateRequestDto.java
+++ user-service/src/main/java/com/example/user_service/dto/UserCreateRequestDto.java
@@ -1,2 +1,34 @@
-package com.example.user_service.dto;public class UserCreateRequestDto {
+package com.example.user_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserCreateRequestDto {
+
+    @NotBlank(message = "Username cannot be null")
+    private String username;
+
+    @Pattern(
+            regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+            message = "Email should match the pattern: test@example.com"
+    )
+    private String email;
+
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "Password should contain the following:\n" +
+                    "- At least 8 characters.\n" +
+                    "- At least one letter.\n" +
+                    "- At least one number.\n" +
+                    "- At least one special symbol."
+    )
+    private String password;
 }
--- user-service/src/main/java/com/example/user_service/dto/UserDto.java
+++ user-service/src/main/java/com/example/user_service/dto/UserDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserResponseDto {
+public class UserDto {
 
     private String id;
 
@@ -20,4 +20,6 @@ public class UserResponseDto {
     private String email;
 
     private LocalDateTime createdAt;
+
+    private LocalDateTime lastModifiedAt;
 }
