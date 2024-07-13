8238d883235acc2bc2513141cb89d8a5822e9d21
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-13 22:01:52 +0300

    created controller tests for user create endpoint

--- user-service/src/test/java/com/example/user_service/cotnroller/UserControllerTest.java
+++ user-service/src/test/java/com/example/user_service/cotnroller/UserControllerTest.java
@@ -1,2 +1,107 @@
-package com.example.user_service.cotnroller;public class UserControllerTest {
+package com.example.user_service.cotnroller;
+
+import com.example.user_service.dto.UserCreateRequestDto;
+import com.example.user_service.dto.UserDto;
+import com.example.user_service.dto.UserResponseDto;
+import com.example.user_service.mappers.UserMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    UserMapper userMapper;
+
+    private UserCreateRequestDto userCreateRequestDto;
+    private UserResponseDto userResponseDto;
+    private UserDto userDto;
+
+    @BeforeEach
+    public void setup() {
+        userCreateRequestDto = UserCreateRequestDto.builder()
+                .username("test")
+                .email("test@example.com")
+                .password("Passss123!")
+                .build();
+
+        userResponseDto = UserResponseDto.builder()
+                .id("uuid")
+                .username("test")
+                .email("test@example.com")
+                .build();
+
+        userDto = UserDto.builder()
+                .username("test")
+                .email("test@example.com")
+                .password("Passss123!")
+                .createdAt(LocalDateTime.now())
+                .lastModifiedAt(LocalDateTime.now())
+                .build();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        userCreateRequestDto = null;
+        userResponseDto = null;
+    }
+
+    @Test
+    void create_shouldReturnCreatedUserData_whenValidRequestData() throws Exception {
+        when(userMapper.toDto(any(UserCreateRequestDto.class))).thenReturn(userDto);
+        when(userMapper.toResponseDto(any(UserDto.class))).thenReturn(userResponseDto);
+        when(userService.create(any(UserDto.class))).thenReturn(userDto);
+
+        MvcResult result = mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userCreateRequestDto))
+        ).andReturn();
+
+        String jsonString = result.getResponse().getContentAsString();
+        UserResponseDto actualResponse = objectMapper.readValue(jsonString, UserResponseDto.class);
+
+        assertThat(actualResponse).isEqualTo(userResponseDto);
+    }
+
+    @Test
+    void create_shouldReturn204Status_whenValidRequestData() throws Exception {
+        when(userMapper.toDto(any(UserCreateRequestDto.class))).thenReturn(userDto);
+        when(userMapper.toResponseDto(any(UserDto.class))).thenReturn(userResponseDto);
+        when(userService.create(any(UserDto.class))).thenReturn(userDto);
+
+        mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userCreateRequestDto))
+        ).andExpect(status().isCreated());
+    }
+
 }
