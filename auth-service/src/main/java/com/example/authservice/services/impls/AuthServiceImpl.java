3e39cd31cdf6b4d7df4dc359199a44b75dc5ca6d
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-22 14:05:32 +0300

    implemented 'register' AuthService method"

--- auth-service/src/main/java/com/example/authservice/services/impls/AuthServiceImpl.java
+++ auth-service/src/main/java/com/example/authservice/services/impls/AuthServiceImpl.java
@@ -6,28 +6,36 @@ import com.example.authservice.dtos.UserRegisterRequestDto;
 import com.example.authservice.dtos.UserRegisterResponseDto;
 import com.example.authservice.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthServiceServiceImpl implements AuthService {
+public class AuthServiceImpl implements AuthService {
 
     private final UserClient userClient;
+    private final ModelMapper modelMapper;
+    private final UserRoleRepository userRoleRepository;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserRegisterResponseDto register(UserRegisterRequestDto request) {
-        var userServiceCreateRequest = UserServiceCreateRequestDto.builder()
-                .username(request.getUsername())
-                .email(request.getEmail())
-                .password(request.getPassword())
+        var userCreateRequest = modelMapper.map(request, UserServiceCreateRequestDto.class);
+        var userCreateResponse = userClient.create(userCreateRequest).getBody();
+
+        Role role = roleRepository.findByName("ROLE_USER");
+
+        assignRolesToUser(userCreateResponse.getId(), role.getId());
+
+        return modelMapper.map(userCreateResponse, UserRegisterResponseDto.class);
+    }
+
+    private void assignRolesToUser(String userId, String roleId) {
+        UserRole userRole = UserRole.builder()
+                .userId(userId)
+                .roleId(roleId)
                 .build();
 
-        var userServiceCreateResponse = userClient.create(userServiceCreateRequest);
-
-        return UserRegisterResponseDto.builder().username(userServiceCreateResponse.getBody().getUsername())
-                .id(userServiceCreateResponse.getBody().getId())
-                .email(userServiceCreateResponse.getBody().getEmail())
-                .createdAt(userServiceCreateResponse.getBody().getCreatedAt())
-                .build();
+        userRoleRepository.save(userRole);
     }
 }
