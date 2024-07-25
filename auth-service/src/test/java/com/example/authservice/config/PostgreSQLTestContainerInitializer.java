aee526dba3d99e2a97cf4c80cc708767b17c9612
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-07-25 20:27:07 +0300

    disabled test database autoconfiguration

--- auth-service/src/test/java/com/example/authservice/config/PostgreSQLTestContainerInitializer.java
+++ auth-service/src/test/java/com/example/authservice/config/PostgreSQLTestContainerInitializer.java
@@ -1,16 +1,16 @@
 package com.example.authservice.config;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 public class PostgreSQLTestContainerInitializer {
 
     @Container
+    @ServiceConnection
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
 
     static {
         postgreSQLContainer.start();
-        var port = postgreSQLContainer.getMappedPort(5342);
-        System.setProperty("postgresql.container.port", port.toString());
     }
 }
--- auth-service/src/test/java/com/example/authservice/repositories/RoleRepositoryTest.java
+++ auth-service/src/test/java/com/example/authservice/repositories/RoleRepositoryTest.java
@@ -1,12 +1,13 @@
 package com.example.authservice.repositories;
 
 import com.example.authservice.config.PostgreSQLTestContainerInitializer;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @DataJpaTest
-@ActiveProfiles("auth-test-containers")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RoleRepositoryTest extends PostgreSQLTestContainerInitializer {
+
 }
