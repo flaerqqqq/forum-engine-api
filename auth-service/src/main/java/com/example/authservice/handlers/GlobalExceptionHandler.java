b3e99506d85cbf3c406669a537490f6dee686dc6
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-08-09 17:56:05 +0300

    implemented exception handler for validation exception

--- auth-service/src/main/java/com/example/authservice/handlers/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/example/authservice/handlers/GlobalExceptionHandler.java
@@ -5,12 +5,18 @@ import com.example.authservice.exceptions.IncorrectPasswordException;
 import com.example.authservice.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleSideExceptions(Exception ex) {
@@ -28,8 +34,24 @@ public class GlobalExceptionHandler {
             UserNotFoundException.class,
             IncorrectPasswordException.class
     })
-    public ResponseEntity<ErrorResponse> handlerBadCredentialsException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(Exception ex) {
         ErrorResponse errorResponse = ErrorResponse.builder(ex, HttpStatus.UNAUTHORIZED,ex.getMessage()).build();
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) throws Exception {
+        Map<String, String> errors = new HashMap<>();
+        for (var errorObject : ex.getBindingResult().getAllErrors()) {
+            if (errorObject instanceof FieldError) {
+                FieldError fieldError = (FieldError) errorObject;
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            } else {
+                errors.put("Error", errorObject.getDefaultMessage());
+            }
+        }
+        ErrorResponse errorResponse = ErrorResponse.builder(
+                ex, HttpStatus.BAD_REQUEST, errors.toString()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
