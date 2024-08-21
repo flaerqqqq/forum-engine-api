8331cc4f582f9c926f8b8bbbb7b9d7ab1ce0eefc
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-08-21 13:42:29 +0300

    implemented openFeign client for log service

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
