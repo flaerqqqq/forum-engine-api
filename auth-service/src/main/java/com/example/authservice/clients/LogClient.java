b11f30170ea76bd9ae42353f7ca823164f5ea90f
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-11 11:24:15 +0300

    created feign client for log service

---
+++ auth-service/src/main/java/com/example/authservice/clients/LogClient.java
@@ -0,0 +1,11 @@
+package com.example.authservice.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "log-service")
+public interface LogClient {
+
+    @PostMapping("/api/v1/logs")
+    void log(LogMessageDto logMessage);
+}
---
+++ auth-service/src/main/java/com/example/authservice/request/LogMessageDto.java
@@ -0,0 +1,29 @@
+package com.example.authservice.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LogMessageDto {
+
+    private LocalDateTime timestamp;
+
+    private String serviceId;
+
+    private String message;
+
+    private String logger;
+
+    private String level;
+
+    private String context;
+
+    private String metadata;
+}
