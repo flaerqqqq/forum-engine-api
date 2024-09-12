b7f5a3a3c4ed9d34a901d2a40b8bcfda9543603f
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 13:04:59 +0300

    documented config-related classes

--- user-service/src/main/java/com/example/user_service/config/MongoConfig.java
+++ user-service/src/main/java/com/example/user_service/config/MongoConfig.java
@@ -4,6 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+/**
+ * Configuration class for MongoDB integration.
+ * <p>
+ * This configuration class sets up MongoDB-specific settings for the application.
+ * It enables MongoDB auditing and repository support:
+ * <ul>
+ *     <li>{@link EnableMongoAuditing} - Enables auditing of MongoDB documents, such as tracking creation and modification times.</li>
+ * </ul>
+ * </p>
+ */
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
