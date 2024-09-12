7a6a56566a4e2dee2c1a6cd7b0d9580db8dd6071
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 13:03:00 +0300

    documented exceptions

--- user-service/src/main/java/com/example/user_service/exceptions/UsernameAlreadyInUseException.java
+++ user-service/src/main/java/com/example/user_service/exceptions/UsernameAlreadyInUseException.java
@@ -1,6 +1,19 @@
 package com.example.user_service.exceptions;
 
+/**
+ * Exception thrown when an attempt is made to register or update a user with a username that is already in use.
+ * <p>
+ * This exception is used to indicate that the username provided for a user already exists in the system
+ * and cannot be used for a new user or updated user record.
+ * </p>
+ */
 public class UsernameAlreadyInUseException extends RuntimeException {
+
+    /**
+     * Constructs an {@code UsernameAlreadyInUseException} with the specified detail message.
+     *
+     * @param msg the detail message
+     */
     public UsernameAlreadyInUseException(String msg) {
         super(msg);
     }
