f4e3c75aec4ac9883d4ec38490805e5976253dfa
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 12:43:33 +0300

    documented OpenFeign clients

--- user-service/src/main/java/com/example/user_service/clients/LogClient.java
+++ user-service/src/main/java/com/example/user_service/clients/LogClient.java
@@ -4,9 +4,26 @@ import com.example.user_service.request.LogMessageDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Feign client interface for interacting with the logging service.
+ * <p>
+ * This interface defines methods for communicating with the {@code log-service},
+ * which is responsible for handling log messages. It uses Feign to simplify
+ * HTTP calls to the log service.
+ * </p>
+ */
 @FeignClient(name = "log-service")
 public interface LogClient {
 
+    /**
+     * Sends a log message to the log service.
+     * <p>
+     * This method posts a {@link LogMessageDto} to the {@code /api/v1/logs} endpoint
+     * of the log service to record log messages.
+     * </p>
+     *
+     * @param logMessage the log message to be sent to the log service
+     */
     @PostMapping("/api/v1/logs")
     void log(LogMessageDto logMessage);
 }
