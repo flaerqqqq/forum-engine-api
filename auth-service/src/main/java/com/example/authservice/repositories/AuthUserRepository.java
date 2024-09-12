51f6cbcbf26110694f9120fd62f03f09865fac80
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 12:33:10 +0300

    documented repositories

--- auth-service/src/main/java/com/example/authservice/repositories/AuthUserRepository.java
+++ auth-service/src/main/java/com/example/authservice/repositories/AuthUserRepository.java
@@ -5,6 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing {@link AuthUser} entities.
+ * <p>
+ * Extends {@link JpaRepository} to provide CRUD operations and custom query methods for {@link AuthUser} entities.
+ * </p>
+ */
 public interface AuthUserRepository extends JpaRepository<AuthUser, String> {
+
+    /**
+     * Finds an {@link AuthUser} by their username.
+     * <p>
+     * This method provides a way to retrieve a user entity based on its username.
+     * </p>
+     *
+     * @param username the username of the user to retrieve
+     * @return an {@link Optional} containing the {@link AuthUser} if found, otherwise {@link Optional#empty()}
+     */
     Optional<AuthUser> findByUsername(String username);
 }
--- auth-service/src/main/java/com/example/authservice/repositories/RoleRepository.java
+++ auth-service/src/main/java/com/example/authservice/repositories/RoleRepository.java
@@ -5,7 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing {@link Role} entities.
+ * <p>
+ * Extends {@link JpaRepository} to provide CRUD operations and custom query methods for {@link Role} entities.
+ * </p>
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    /**
+     * Finds a {@link Role} by its name.
+     * <p>
+     * This method provides a way to retrieve a role entity based on its name.
+     * </p>
+     *
+     * @param roleName the name of the role to retrieve, encapsulated in {@link Role.RoleName}
+     * @return an {@link Optional} containing the {@link Role} if found, otherwise {@link Optional#empty()}
+     */
     Optional<Role> findByName(Role.RoleName roleName);
 }
