9b6281b2cdc77f6051b4c3d006be0cfcb93d20c6
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 12:28:49 +0300

    documented OpenFeign clients

--- auth-service/src/main/java/com/example/authservice/clients/LogClient.java
+++ auth-service/src/main/java/com/example/authservice/clients/LogClient.java
@@ -4,9 +4,26 @@ import com.example.authservice.request.LogMessageDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Feign client for interacting with the external log service.
+ * This client sends log messages to the "log-service" microservice.
+ * <p>
+ * - Uses {@link FeignClient} to communicate with the "log-service".
+ * - Sends log messages to the endpoint "/api/v1/logs".
+ * </p>
+ *
+ * Annotations:
+ * - {@code @FeignClient}: Indicates that this interface is a Feign client for the "log-service" microservice.
+ */
 @FeignClient(name = "log-service")
 public interface LogClient {
 
+    /**
+     * Sends a log message to the external log service.
+     * This method performs a POST request to the "/api/v1/logs" endpoint of the "log-service".
+     *
+     * @param logMessage the log message to be sent, encapsulated in a {@link LogMessageDto}
+     */
     @PostMapping("/api/v1/logs")
     void log(LogMessageDto logMessage);
 }
--- auth-service/src/main/java/com/example/authservice/clients/UserClient.java
+++ auth-service/src/main/java/com/example/authservice/clients/UserClient.java
@@ -10,12 +10,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * Feign client for interacting with the "user-service" microservice.
+ * This client provides methods for retrieving and creating user records in the "user-service".
+ * <p>
+ * - Uses {@link FeignClient} to communicate with the "user-service".
+ * - Configuration is provided by {@link FeignUserServiceConfig}.
+ * - The base URL for the service is specified in the application configuration.
+ * </p>
+ *
+ * Annotations:
+ * - {@code @FeignClient}: Indicates that this interface is a Feign client for the "user-service".
+ * - {@code @GetMapping}: Maps to the GET request for retrieving user details.
+ * - {@code @PostMapping}: Maps to the POST request for creating a new user.
+ */
 @FeignClient(name = "user-service", url = "${application.config.user-url}", configuration = FeignUserServiceConfig.class)
 public interface UserClient {
 
+    /**
+     * Retrieves a user by ID from the "user-service".
+     * This method performs a GET request to the "/{id}" endpoint of the "user-service".
+     *
+     * @param id the ID of the user to retrieve
+     * @return a {@link ResponseEntity} containing the {@link UserServiceResponseDto} with user details
+     */
     @GetMapping("/{id}")
     ResponseEntity<UserServiceResponseDto> getById(@PathVariable String id);
 
+    /**
+     * Creates a new user in the "user-service".
+     * This method performs a POST request to the root endpoint of the "user-service" with the user creation request body.
+     *
+     * @param request the user creation request encapsulated in a {@link UserServiceCreateRequestDto}
+     * @return a {@link ResponseEntity} containing the {@link UserServiceResponseDto} with the created user's details
+     */
     @PostMapping
     ResponseEntity<UserServiceResponseDto> create(@RequestBody UserServiceCreateRequestDto request);
 }
