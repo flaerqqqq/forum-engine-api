4ce4c2212e65e656b5ef59a82d2695f09d13e5f1
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-09-12 13:11:31 +0300

    documented config-related classes

--- auth-service/src/main/java/com/example/authservice/config/BeanConfig.java
+++ auth-service/src/main/java/com/example/authservice/config/BeanConfig.java
@@ -1,13 +1,28 @@
 package com.example.authservice.config;
 
-import feign.codec.ErrorDecoder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for defining application-wide beans.
+ * <p>
+ * This class provides configuration for beans used throughout the application.
+ * Currently, it configures a {@link ModelMapper} bean to facilitate object mapping.
+ * </p>
+ */
 @Configuration
 public class BeanConfig {
 
+    /**
+     * Creates and returns a {@link ModelMapper} bean.
+     * <p>
+     * The {@link ModelMapper} is used for mapping between different object models,
+     * simplifying the process of converting data between different formats or structures.
+     * </p>
+     *
+     * @return a new instance of {@link ModelMapper}
+     */
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
